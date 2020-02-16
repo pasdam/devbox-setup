5277776dccbe482c19630ede165eb24f72f866f6
pasdam <pasdam@users.noreply.github.com>
2020-02-16 14:26:15 +0800

    Updated VSCode settings, and fix setup script

--- config/vscode/setup.sh
+++ config/vscode/setup.sh
@@ -3,21 +3,21 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 VSCODE_CONFIG_DIR="$HOME/Library/Application Support/VSCodium/User"
 
-if [ -f "$SCRIPT_DIR/keybindings.json" ]; then
-    echo "$SCRIPT_DIR/keybindings.json already exists, the setup won't override it"
+if [ -f "$VSCODE_CONFIG_DIR/keybindings.json" ]; then
+    echo "$VSCODE_CONFIG_DIR/keybindings.json already exists, the setup won't override it"
 
 else 
-    ln -sf  "$VSCODE_CONFIG_DIR/keybindings.json"
+    ln -sf "$SCRIPT_DIR/keybindings.json" "$VSCODE_CONFIG_DIR/keybindings.json"
 fi
 
-if [ -f "$SCRIPT_DIR/settings.json" ]; then
-    echo "$SCRIPT_DIR/settings.json already exists, the setup won't override it"
+if [ -f "$VSCODE_CONFIG_DIR/settings.json" ]; then
+    echo "$VSCODE_CONFIG_DIR/settings.json already exists, the setup won't override it"
 else 
-    ln -sf $SCRIPT_DIR/settings.json "$VSCODE_CONFIG_DIR/settings.json"
+    ln -sf "$SCRIPT_DIR/settings.json" "$VSCODE_CONFIG_DIR/settings.json"
 fi
 
-if [ -d "$SCRIPT_DIR/snippets" ]; then
-    echo "$SCRIPT_DIR/snippets already exists, the setup won't override it"
+if [ -d "$VSCODE_CONFIG_DIR/snippets" ]; then
+    echo "$VSCODE_CONFIG_DIR/snippets already exists, the setup won't override it"
 else 
-    ln -sf $SCRIPT_DIR/snippets "$VSCODE_CONFIG_DIR/snippets"
+    ln -sf "$SCRIPT_DIR/snippets" "$VSCODE_CONFIG_DIR/snippets"
 fi
