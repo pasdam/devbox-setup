c6eda3a2bf60e64d6ece4474163711fb80a9f34e
pasdam <pasdam@users.noreply.github.com>
2020-05-31 13:06:14 +0800

    Add raspbian initial setup script

---
+++ raspbian/k8s.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# install k8s
+DIR=~/workspace/github/teamserverless
+mkdir -p $DIR
+cd $DIR
+git clone https://github.com/teamserverless/k8s-on-raspbian.git --depth 1
+chmod +x $DIR/k8s-on-raspbian/script/prep.sh
+$DIR/k8s-on-raspbian/script/prep.sh
+
+# enable docker daemon to receive tcp requests
+sudo cp /lib/systemd/system/docker.service /lib/systemd/system/docker.service.bkp
+EXEC_START=$(cat /lib/systemd/system/docker.service | grep "ExecStart=/usr/bin/dockerd")
+
+if [ ! -z "${EXEC_START##*-H tcp://0.0.0.0:2375*}" ]; then
+    NEW_EXEC_START=$(echo $EXEC_START | xargs printf "%s -H tcp://0.0.0.0:2375")
+    # TODO: replace lines
+fi
