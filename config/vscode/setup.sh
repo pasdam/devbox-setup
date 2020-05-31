d636ee0474ca7761cf72458ef5f9a6d9e1e09398
pasdam <pasdam@users.noreply.github.com>
2020-05-31 13:02:08 +0800

    Use portable shebang

--- config/vscode/setup.sh
+++ config/vscode/setup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 VSCODE_CONFIG_DIR="$HOME/Library/Application Support/VSCodium/User"
@@ -6,18 +6,18 @@ VSCODE_CONFIG_DIR="$HOME/Library/Application Support/VSCodium/User"
 if [ -f "$VSCODE_CONFIG_DIR/keybindings.json" ]; then
     echo "$VSCODE_CONFIG_DIR/keybindings.json already exists, the setup won't override it"
 
-else 
+else
     ln -sf "$SCRIPT_DIR/keybindings.json" "$VSCODE_CONFIG_DIR/keybindings.json"
 fi
 
 if [ -f "$VSCODE_CONFIG_DIR/settings.json" ]; then
     echo "$VSCODE_CONFIG_DIR/settings.json already exists, the setup won't override it"
-else 
+else
     ln -sf "$SCRIPT_DIR/settings.json" "$VSCODE_CONFIG_DIR/settings.json"
 fi
 
 if [ -d "$VSCODE_CONFIG_DIR/snippets" ]; then
     echo "$VSCODE_CONFIG_DIR/snippets already exists, the setup won't override it"
-else 
+else
     ln -sf "$SCRIPT_DIR/snippets" "$VSCODE_CONFIG_DIR/snippets"
 fi
