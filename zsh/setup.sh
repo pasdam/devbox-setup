fd062fca314cb9f1785b6843c8108bf06276ca36
pasdam <pasdam@users.noreply.github.com>
2020-06-01 10:46:03 +0800

    Make a backup of zshrc, make k8s. setup script executable

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -3,6 +3,7 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # install dotfiles
+mv $HOME/.zshrc $HOME/.zshrc.bkp
 ln -sf $SCRIPT_DIR/.zshrc $HOME/.zshrc
 
 $SCRIPT_DIR/oh-my-zsh/setup.sh
