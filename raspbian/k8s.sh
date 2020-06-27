7b3c63df762ef98ff2b946840b9cbfec8542ca98
pasdam <pasdam@users.noreply.github.com>
2020-06-27 09:19:00 +0800

    Simplify k8s script on rasbian

--- raspbian/k8s.sh
+++ raspbian/k8s.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
 # install k8s
-DIR=~/workspace/github/teamserverless
-mkdir -p $DIR
-cd $DIR
-git clone https://github.com/teamserverless/k8s-on-raspbian.git --depth 1
-chmod +x $DIR/k8s-on-raspbian/script/prep.sh
-$DIR/k8s-on-raspbian/script/prep.sh
+# DIR=~/workspace/github/teamserverless
+# mkdir -p $DIR
+# cd $DIR
+# git clone https://github.com/teamserverless/k8s-on-raspbian.git --depth 1
+# chmod +x $DIR/k8s-on-raspbian/script/prep.sh
+# $DIR/k8s-on-raspbian/script/prep.sh
+
+curl -sfL https://get.k3s.io | sh -
 
 # enable docker daemon to receive tcp requests
 sudo cp /lib/systemd/system/docker.service /lib/systemd/system/docker.service.bkp
